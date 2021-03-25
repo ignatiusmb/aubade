af0f0090687485c8049ffb3b5b7351315f9eca48
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-03-25 11:27:38 +0700

    feat(parser): rationalize return items and expose some metadata (#1)
    * feat: filter undefined parseDir items
    
    * refactor: prefer match over regexp exec
    
    * feat: expose toc and read_time to hydrate callback

--- src/internal/parser.ts
+++ src/internal/parser.ts
@@ -1,26 +1,25 @@
 import type { HydrateFn } from './types';
 import { join } from 'path';
 import { readdirSync, readFileSync } from 'fs';
-import { compareDate, compareString } from './helper';
+import { isExists } from 'mauss/guards';
+import { compareString } from './helper';
 import { contentParser, countReadTime, extractMeta, generateTable, traverseCompare } from './utils';
 import marker from './marker';
 
 export function parseFile<I, O = I>(pathname: string, hydrate: HydrateFn<I, O>): O;
 export function parseFile<I, O = I>(pathname: string, hydrate: HydrateFn<I, O>): O | undefined {
-	const content = readFileSync(pathname, 'utf-8');
-	const fmExpression = /---\r?\n([\s\S]+?)\r?\n---/;
-	const [rawData, metadata] = fmExpression.exec(content) || ['', ''];
-
-	const extracted = extractMeta(metadata);
+	const content = readFileSync(pathname, 'utf-8').trim();
+	const match = content.match(/---\r?\n([\s\S]+?)\r?\n---/);
 	const [filename] = pathname.split(/[/\\]/).slice(-1);
-	const article = metadata ? content.slice(rawData.length + 1) : content;
-	const result = <typeof extracted>(
-		hydrate({ frontMatter: <I>extracted, content: article, filename })
-	);
+
+	const metadata = extractMeta((match && match[1].trim()) || '');
+	const sliceIdx = match ? (match.index || 0) + match[0].length + 1 : 0;
+	const article = !match ? content : content.slice(sliceIdx);
+	metadata.toc = generateTable(article);
+	metadata.read_time = countReadTime(article);
+	const result = <typeof metadata>hydrate({ frontMatter: <I>metadata, content: article, filename });
 	if (!result) return;
 
-	result.toc = generateTable(article);
-	result.read_time = countReadTime(article);
 	if (result.date && result.date.published && !result.date.updated) {
 		result.date.updated = result.date.published;
 	}
@@ -33,22 +32,23 @@ export function parseFile<I, O = I>(pathname: string, hydrate: HydrateFn<I, O>):
 	return result as O;
 }
 
-export function parseDir<I, O = I>(dirname: string, hydrate: HydrateFn<I, O>): O[];
+export function parseDir<I, O = I>(dirname: string, hydrate: HydrateFn<I, O>): Array<O>;
 export function parseDir<I, O extends Record<string, any> = I>(
 	dirname: string,
 	hydrate: HydrateFn<I, O>
-): Array<O | undefined> {
+): Array<O> {
 	return readdirSync(dirname)
 		.filter((name) => !name.startsWith('draft.') && name.endsWith('.md'))
 		.map((filename) => parseFile(join(dirname, filename), hydrate))
+		.filter(isExists)
 		.sort((x, y) => {
 			if (x.date && y.date) {
 				if (typeof x.date === 'string' && typeof y.date === 'string')
 					if (x.date !== y.date) return compareString(x.date, y.date);
 				const { updated: xu, published: xp } = x.date;
 				const { updated: yu, published: yp } = y.date;
-				if (xu && yu && xu !== yu) return compareDate(xu, yu);
-				if (xp && yp && xp !== yp) return compareDate(xp, yp);
+				if (xu && yu && xu !== yu) return compareString(xu, yu);
+				if (xp && yp && xp !== yp) return compareString(xp, yp);
 			}
 			return traverseCompare(x, y);
 		});
