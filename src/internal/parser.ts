04af1142c83835946fbf8b8205d328391546c106
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-04-08 13:06:55 +0700

    feat: add minimal and exclude options

--- src/internal/parser.ts
+++ src/internal/parser.ts
@@ -1,4 +1,4 @@
-import type { HydrateFn } from './types';
+import type { DirOptions, FileOptions, HydrateFn } from './types';
 import { join } from 'path';
 import { existsSync, readdirSync, readFileSync } from 'fs';
 import { isExists } from 'mauss/guards';
@@ -7,9 +7,12 @@ import { contentParser, countReadTime, extractMeta, generateTable, traverseCompa
 import marker from './marker';
 
 export function parseFile<I, O extends Record<string, any> = I>(
-	pathname: string,
+	options: string | FileOptions,
 	hydrate?: HydrateFn<I, O>
 ): O | undefined {
+	const { pathname, minimal = !0, exclude = [] } =
+		typeof options !== 'string' ? options : { pathname: options };
+
 	const crude = readFileSync(pathname, 'utf-8').trim();
 	const match = crude.match(/---\r?\n([\s\S]+?)\r?\n---/);
 	const [filename] = pathname.split(/[/\\]/).slice(-1);
@@ -17,30 +20,34 @@ export function parseFile<I, O extends Record<string, any> = I>(
 	const metadata = extractMeta((match && match[1].trim()) || '');
 	const sliceIdx = match ? (match.index || 0) + match[0].length + 1 : 0;
 	const content = contentParser(metadata, crude.slice(sliceIdx));
-	metadata.toc = generateTable(content);
-	metadata.read_time = countReadTime(content);
+	if (!minimal) {
+		if (!exclude.includes('toc')) metadata.toc = generateTable(content);
+		if (!exclude.includes('rt')) metadata.read_time = countReadTime(content);
+	}
 	const result = !hydrate
 		? ({ ...metadata, content } as Record<string, any>)
 		: hydrate({ frontMatter: <I>metadata, content, filename });
 
 	if (!result) return;
 
-	if (result.date) result.date.updated = result.date.updated || result.date.published || undefined;
+	if (result.date && !minimal && !exclude.includes('date'))
+		result.date.updated = result.date.updated || result.date.published || undefined;
 	if (result.content) result.content = marker.render(result.content);
 
 	return result as O;
 }
 
 export function parseDir<I, O extends Record<string, any> = I>(
-	options: string | { dirname: string; extensions?: string[] },
+	options: string | (DirOptions & FileOptions),
 	hydrate?: HydrateFn<I, O>
 ): Array<O> {
-	const { dirname, extensions = ['.md'] } =
-		typeof options === 'string' ? { dirname: options } : options;
+	const { dirname, extensions = ['.md'], ...config } =
+		typeof options !== 'string' ? options : { dirname: options };
+
 	if (!existsSync(dirname)) throw new Error(`Pathname ${dirname} does not exists!`);
 	return readdirSync(dirname)
 		.filter((name) => !name.startsWith('draft.') && extensions.some((ext) => name.endsWith(ext)))
-		.map((filename) => parseFile(join(dirname, filename), hydrate))
+		.map((filename) => parseFile({ pathname: join(dirname, filename), ...config }, hydrate))
 		.filter(isExists)
 		.sort((x, y) => {
 			if (x.date && y.date) {
