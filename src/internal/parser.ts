97b643af88ddf093bd2ad7729b224b28b7849f08
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-04-09 12:16:41 +0700

    feat(parser): directory and file options (#3)
    * feat: allow object to be passed as first argument
    
    * feat: throw error if directory DNE
    
    * feat: make hydrate function optional
    
    * perf: simplify and remove duplicate content parser
    
    * feat: add minimal and exclude options
    
    * refactor: remove unnecessary undefined
    
    * bump patch version

--- src/internal/parser.ts
+++ src/internal/parser.ts
@@ -1,13 +1,18 @@
-import type { HydrateFn } from './types';
+import type { DirOptions, FileOptions, HydrateFn } from './types';
 import { join } from 'path';
-import { readdirSync, readFileSync } from 'fs';
+import { existsSync, readdirSync, readFileSync } from 'fs';
 import { isExists } from 'mauss/guards';
 import { compareString } from './helper';
 import { contentParser, countReadTime, extractMeta, generateTable, traverseCompare } from './utils';
 import marker from './marker';
 
-export function parseFile<I, O = I>(pathname: string, hydrate: HydrateFn<I, O>): O;
-export function parseFile<I, O = I>(pathname: string, hydrate: HydrateFn<I, O>): O | undefined {
+export function parseFile<I, O extends Record<string, any> = I>(
+	options: string | FileOptions,
+	hydrate?: HydrateFn<I, O>
+): O | undefined {
+	const { pathname, minimal = !0, exclude = [] } =
+		typeof options !== 'string' ? options : { pathname: options };
+
 	const crude = readFileSync(pathname, 'utf-8').trim();
 	const match = crude.match(/---\r?\n([\s\S]+?)\r?\n---/);
 	const [filename] = pathname.split(/[/\\]/).slice(-1);
@@ -15,38 +20,41 @@ export function parseFile<I, O = I>(pathname: string, hydrate: HydrateFn<I, O>):
 	const metadata = extractMeta((match && match[1].trim()) || '');
 	const sliceIdx = match ? (match.index || 0) + match[0].length + 1 : 0;
 	const content = contentParser(metadata, crude.slice(sliceIdx));
-	metadata.toc = generateTable(content);
-	metadata.read_time = countReadTime(content);
-	const result = <typeof metadata>hydrate({ frontMatter: <I>metadata, content, filename });
+	if (!minimal) {
+		if (!exclude.includes('toc')) metadata.toc = generateTable(content);
+		if (!exclude.includes('rt')) metadata.read_time = countReadTime(content);
+	}
+	const result = !hydrate
+		? ({ ...metadata, content } as Record<string, any>)
+		: hydrate({ frontMatter: <I>metadata, content, filename });
+
 	if (!result) return;
 
-	if (result.date && result.date.published && !result.date.updated) {
-		result.date.updated = result.date.published;
-	}
+	if (result.date && !minimal && !exclude.includes('date'))
+		result.date.updated = result.date.updated || result.date.published;
+	if (result.content) result.content = marker.render(result.content);
 
-	if (result.content) {
-		const { content, ...rest } = result;
-		result.content = contentParser(rest, content);
-		result.content = marker.render(result.content);
-	}
 	return result as O;
 }
 
-export function parseDir<I, O = I>(dirname: string, hydrate: HydrateFn<I, O>): Array<O>;
 export function parseDir<I, O extends Record<string, any> = I>(
-	dirname: string,
-	hydrate: HydrateFn<I, O>
+	options: string | (DirOptions & FileOptions),
+	hydrate?: HydrateFn<I, O>
 ): Array<O> {
+	const { dirname, extensions = ['.md'], ...config } =
+		typeof options !== 'string' ? options : { dirname: options };
+
+	if (!existsSync(dirname)) throw new Error(`Pathname ${dirname} does not exists!`);
 	return readdirSync(dirname)
-		.filter((name) => !name.startsWith('draft.') && name.endsWith('.md'))
-		.map((filename) => parseFile(join(dirname, filename), hydrate))
+		.filter((name) => !name.startsWith('draft.') && extensions.some((ext) => name.endsWith(ext)))
+		.map((filename) => parseFile({ pathname: join(dirname, filename), ...config }, hydrate))
 		.filter(isExists)
 		.sort((x, y) => {
 			if (x.date && y.date) {
 				if (typeof x.date === 'string' && typeof y.date === 'string')
 					if (x.date !== y.date) return compareString(x.date, y.date);
-				const { updated: xu, published: xp } = x.date;
-				const { updated: yu, published: yp } = y.date;
+				const { updated: xu = '', published: xp = '' } = x.date;
+				const { updated: yu = '', published: yp = '' } = y.date;
 				if (xu && yu && xu !== yu) return compareString(xu, yu);
 				if (xp && yp && xp !== yp) return compareString(xp, yp);
 			}
