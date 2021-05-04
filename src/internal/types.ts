7e3fbabca37cb12fe54895bedee75d3f8b8bdf94
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-05-04 21:04:27 +0700

    narrow types even more

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -28,15 +28,19 @@ export interface MarquaTable {
 
 /** Parser Types */
 export interface FrontMatter {
-	toc?: Array<MarquaTable>;
-	read_time?: number;
+	toc: Array<MarquaTable>;
+	read_time: number;
 	content?: string | Array<MarquaData>;
 	date: Record<'created' | 'modified', Date> &
 		Partial<Record<'published' | 'updated', string | Date>>;
 }
 export type HydrateFn<Options extends FileOptions, Input, Output = Input> = (chunk: {
 	frontMatter: keyof Input extends never
-		? Omit<FrontMatter, 'content'> & Record<string, any>
+		? Options['minimal'] extends true
+			? Pick<FrontMatter, 'date'> & Record<string, any>
+			: Omit<FrontMatter, 'content'> & Record<string, any>
+		: Options['minimal'] extends true
+		? Pick<FrontMatter, 'date'> & Input
 		: Omit<FrontMatter, 'content' | keyof Input> & Input;
 	content: Options['minimal'] extends true ? string : Array<MarquaData>;
 	breadcrumb: Array<string>;
