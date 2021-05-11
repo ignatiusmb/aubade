6133cb03a1885f25512796b5ffa40ae80a6d5767
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-05-11 18:55:14 +0700

    test: initial type definitions checks (#13)
    * fix: change Input check for non-empty object type
    
    * use built-in Omit utility type
    
    * revert Input extends back to Record
    
    * chore: ignore all branches
    
    * chore: install before publishing
    
    * add tsd package
    
    * specify lib in tsconfig
    
    * rename yml -> yaml
    
    * add lint prefixed scripts
    
    * add quality-ci workflow
    
    * add initial index spec
    
    * revert Input type back to object
    
    * add options type tests
    
    * narrow types even more
    
    * add more main types test
    
    * rename test files extension
    
    * npm audit fix
    
    * workaround tsd needing .d.ts
    
    * add more type tests
    
    * chore: explicitly specify include files
    
    * refactor and break up tests
    
    * split up default-string to individual methods
    
    * remove index covered by default-string
    
    * use more explicit strings
    
    * rename folder
    
    * kiss

---
+++ test/typings/core/default-string/compile.test-d.ts
@@ -0,0 +1,28 @@
+/**
+ * input: string
+ * generics: none
+ */
+
+import type * as ts from '../../../../src/internal/types';
+import { compile } from '../../../../src';
+import { expectAssignable, expectType } from 'tsd';
+
+const entry = 'nonexistent-folder';
+
+type Generic = Record<string, any>;
+type Default = Omit<ts.FrontMatter, 'content'> & Generic;
+
+expectType<void | Generic>(
+	compile(entry, ({ frontMatter, content, breadcrumb }) => {
+		expectType<Default>(frontMatter);
+		expectType<Array<ts.MarquaTable>>(frontMatter.toc);
+		expectType<number>(frontMatter.read_time);
+		expectAssignable<object>(frontMatter.date);
+		expectType<Date>(frontMatter.date.created);
+		expectType<Date>(frontMatter.date.modified);
+
+		expectType<Array<ts.MarquaData>>(content);
+
+		expectType<Array<string>>(breadcrumb);
+	})
+);
