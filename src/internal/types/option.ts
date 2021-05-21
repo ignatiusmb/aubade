505a5e0e574126bbd6f0c830596708c73c01cbcc
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-05-21 12:08:20 +0700

    revert 2821fd146de38ed7fd1779a0d3f92bd09232d10b

--- src/internal/types/option.ts
+++ src/internal/types/option.ts
@@ -1,8 +1,6 @@
-declare const UnSym1: unique symbol;
-declare const UnSym2: unique symbol;
 export type ParserTypes<Input, Output> = {
-	[UnSym1]: Input;
-	[UnSym2]: Output;
+	UnSym(): Input;
+	UnSym(): Output;
 };
 
 export type FileOptions = {
