21faf1c8a8bb97ce6b721ec48a19d7d24820c548
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-03-29 13:47:37 +0700

    fix path normalization

--- test/apps/utils.ts
+++ test/apps/utils.ts
@@ -2,7 +2,6 @@ import fs from 'fs';
 import path from 'path';
 
 export function readJSON(pathname: string) {
-	const inverse = new RegExp(path.sep === '/' ? '\\' : '/', 'g');
-	const normalized = pathname.replace(inverse, path.sep);
-	return JSON.parse(fs.readFileSync(normalized, 'utf-8'));
+	if (path.sep !== '/') pathname = pathname.replace(/\//g, path.sep);
+	return JSON.parse(fs.readFileSync(pathname, 'utf-8'));
 }
