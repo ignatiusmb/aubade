0d84823277abf455b4e0d26c4c9f9758e1e4c9b8
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-09-26 17:27:45 +0700

    use HydrateChunk type

--- src/internal/index.ts
+++ src/internal/index.ts
@@ -1,4 +1,4 @@
-import type { DirOptions, FileOptions, FrontMatter, Hydrate, ParserTypes } from './types.js';
+import type * as TS from './types.js';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -6,13 +6,13 @@ import { readTime, structure, table } from './compute.js';
 import { construct, supplant } from './utils.js';
 
 export function compile<
-	Options extends FileOptions,
+	Options extends TS.FileOptions,
 	Input extends object,
 	Output extends Record<string, any> = Input
 >(
 	options: string | Options,
-	hydrate?: Hydrate<Options, Input, Output>,
-	_types?: ParserTypes<Input, Output>
+	hydrate?: TS.Hydrate<Options, Input, Output>,
+	_types?: TS.ParserTypes<Input, Output>
 ): undefined | Output {
 	const {
 		entry,
@@ -38,8 +38,8 @@ export function compile<
 	}
 	const chunk = { frontMatter: metadata, content, breadcrumb };
 	const result = !hydrate
-		? ({ ...metadata, content } as FrontMatter)
-		: hydrate(chunk as Parameters<typeof hydrate>[0]);
+		? ({ ...metadata, content } as TS.FrontMatter)
+		: hydrate(chunk as TS.HydrateChunk<Options, Input>);
 
 	if (!result /* hydrate is used and returns nothing */) return;
 
@@ -54,13 +54,13 @@ export function compile<
 }
 
 export function traverse<
-	Options extends DirOptions<Output>,
+	Options extends TS.DirOptions<Output>,
 	Input extends object,
 	Output extends Record<string, any> = Input
 >(
 	options: string | Options,
-	hydrate?: Hydrate<Options, Input, Output>,
-	_types?: ParserTypes<Input, Output>
+	hydrate?: TS.Hydrate<Options, Input, Output>,
+	_types?: TS.ParserTypes<Input, Output>
 ): Array<Output> {
 	const {
 		entry,
