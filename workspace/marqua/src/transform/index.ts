9ecb406717583d4e01a9b16553d5748bd8508461
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-01-11 10:50:29 +0700

    bump deps

--- workspace/marqua/src/transform/index.ts
+++ workspace/marqua/src/transform/index.ts
@@ -1,22 +1,6 @@
-import type { Primitives } from 'mauss/typings';
 import { ntv } from 'mauss/std';
 
 export { pipe } from 'mauss';
-
-type Narrowable = Primitives | symbol | object | undefined | void | null | {};
-type Fallback<A, B> = A extends B ? A : B;
-type Narrow<T> = Fallback<
-	T,
-	[] | (T extends Narrowable ? T : never) | { [K in keyof T]: Narrow<T[K]> }
->;
-
-// TODO remove once merged - https://github.com/alchemauss/mauss/pull/199
-function pick<T extends readonly string[]>(keys: Narrow<T>) {
-	const props = new Set(keys);
-	return (o: { [K in T[number]]?: any }) => {
-		return ntv.iterate(o, ([k, v]) => props.has(k) && [k, v]);
-	};
-}
 
 interface ChainOptions<T> {
 	base?: string;
@@ -29,7 +13,7 @@ export function chain<T extends { slug: string; title: string; [k: string]: any 
 	type Attachment = { flank?: { back?: T; next?: T } };
 	return (items: T[]): Array<T & Attachment> => {
 		const array: Array<T & Attachment> = items;
-		const unwrap = pick(['slug', 'title']);
+		const unwrap = ntv.pick(['slug', 'title']);
 		for (let idx = 0; idx < array.length; idx++) {
 			const [back, next] = [array[idx - 1], array[idx + 1]];
 			if (back) {
