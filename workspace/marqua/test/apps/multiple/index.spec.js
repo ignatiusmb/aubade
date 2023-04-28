e732ac0e55152c895e392de9d2a550b042823d00
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-04-28 16:21:58 +0700

    test

--- workspace/marqua/test/apps/multiple/index.spec.js
+++ workspace/marqua/test/apps/multiple/index.spec.js
@@ -5,6 +5,7 @@ import { readJSON } from '../utils.js';
 
 const basics = {
 	standard: suite('multiple:standard'),
+	depth: suite('multiple:depth'),
 };
 
 const target = `${process.cwd()}/test/apps/multiple`;
@@ -12,6 +13,16 @@ const target = `${process.cwd()}/test/apps/multiple`;
 basics.standard('standard traversal', () => {
 	const output = traverse({ entry: `${target}/standard/input` });
 	const expected = readJSON(`${target}/standard/expected.json`);
+
+	assert.type(output, 'object');
+	assert.type(expected, 'object');
+
+	assert.equal(output, expected);
+});
+
+basics.depth('depth traversal', () => {
+	const output = traverse({ entry: `${target}/depth/input`, depth: 1 });
+	const expected = readJSON(`${target}/depth/expected.json`);
 
 	assert.type(output, 'object');
 	assert.type(expected, 'object');
