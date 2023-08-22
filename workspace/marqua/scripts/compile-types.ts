48494068f58fc0b048133a8c6c0376076fb1852a
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-08-22 13:33:28 +0700

    remove if exists

--- workspace/marqua/scripts/compile-types.ts
+++ workspace/marqua/scripts/compile-types.ts
@@ -1,5 +1,8 @@
+import * as fs from 'node:fs';
 import { createBundle } from 'dts-buddy';
 import { exports } from '../package.json';
+
+walk('./src', (path) => path.endsWith('.js') && fs.unlinkSync(path));
 
 await createBundle({
 	output: 'index.d.ts',
@@ -9,3 +12,16 @@ await createBundle({
 		return { ...acc, ['marqua' + key.slice(1)]: exports[key] };
 	}, {}),
 });
+
+// ---- helper functions ----
+
+function walk(entry: string, fn: (pathname: string) => void) {
+	for (const name of fs.readdirSync(entry)) {
+		const path = `${entry}/${name}`;
+		if (fs.statSync(path).isDirectory()) {
+			walk(path, fn);
+		} else {
+			fn(path);
+		}
+	}
+}
