1f8d6d11d85f249b0348c4af5e6097bfd2addcb7
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-12-20 14:23:43 +0700

    feat: use `shikiji` (#104)

--- workspace/documentation/vite.config.js
+++ workspace/documentation/vite.config.js
@@ -4,17 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 
-	build: {
-		rollupOptions: {
-			external: ['marqua/fs'],
-		},
-	},
-
 	server: {
 		port: 3000,
-	},
-
-	ssr: {
-		external: ['markdown-it', 'shiki'],
 	},
 });
