e6543d2556170682c1ebdb57f0ab97ae89014912
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-01-04 20:24:07 +0700

    docs: refactor workspace and content

--- workspace/marqua/src/artisan/index.js
+++ workspace/marqua/src/artisan/index.js
@@ -1,6 +1,2 @@
 export { highlighter, transform } from './brush.js';
 export { marker } from './marker.js';
-
-/**
- * @typedef {import('./brush.js').Dataset} Dataset
- */
--- workspace/website/src/routes/+page.server.ts
+++ workspace/website/src/routes/+page.server.ts
@@ -6,12 +6,12 @@ export const prerender = true;
 
 export const load: import('./$types').PageServerLoad = async () => {
 	const docs: ComponentProps<Docs>['sections'] = traverse(
-		{ entry: '../../content' },
-		({ breadcrumb: [filename], buffer, parse }) => {
-			const path = `content/${filename}`;
-			const [, index, slug] = filename.match(/^(\d{2})-(.+).md$/)!;
+		{ entry: '../content' },
+		({ breadcrumb: [filename], buffer, marker, parse }) => {
+			const path = `workspace/content/${filename}`;
+			const slug = filename.match(/^(\d{2})-(.+).md$/)![2];
 			const { content, metadata } = parse(buffer.toString('utf-8'));
-			return { index, slug, title: metadata.title, path, content };
+			return { slug, title: metadata.title, path, content: marker.render(content) };
 		},
 	);
 
