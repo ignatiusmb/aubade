9f17d14d5d54e7f4b5b4522bb2ae2bfd48712cc7
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-01-16 15:28:08 +0700

    try handling initial fetch

--- workspace/website/src/routes/docs/[slug]/+page.server.ts
+++ workspace/website/src/routes/docs/[slug]/+page.server.ts
@@ -2,7 +2,10 @@ import type { Schema } from '../content.json/+server';
 import { error } from '@sveltejs/kit';
 
 export async function load({ fetch, params }) {
-	const { items, metadata }: Schema = await fetch('/docs/content.json').then((r) => r.json());
+	const res = await fetch('/docs/content.json');
+	if (!res.ok) error(500, 'Could not load content');
+
+	const { items, metadata }: Schema = await res.json();
 	const docs = items.find(({ slug }) => slug === params.slug);
 	if (!docs) error(404, "Sorry, we can't find that page");
 
