6ce509d38b4c01368517f27e93db4eb42cd754e3
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-03-12 20:05:56 +0700

    feat: simplify transform module (#135)

--- workspace/aubade/src/transform/index.js
+++ workspace/aubade/src/transform/index.js
@@ -4,14 +4,14 @@ export { pipe } from 'mauss';
 
 /**
  * @template {{ slug?: string; title?: any }} T
+ * @param {T[]} items
  * @param {{
  * 	base?: string
  * 	breakpoint?(next: T): boolean
  * 	sort?(x: T, y: T): number
  * }} options
- * @returns
  */
-export function chain(options) {
+export function chain(items, options) {
 	const { base = '', breakpoint, sort } = options;
 
 	/**
@@ -20,27 +20,24 @@ export function chain(options) {
 	 * @typedef {{ flank?: Flank }} Attachment
 	 */
 
-	/** @type {(items: T[]) => Array<T & Attachment>} */
-	return (items) => {
-		if (sort) items = items.sort(sort);
-		const array = /** @type {Array<T & Attachment>} */ (items);
-		const unwrap = ntv.pick(['slug', 'title']);
-		for (let idx = 0; idx < array.length; idx++) {
-			const [back, next] = [array[idx - 1], array[idx + 1]];
-			if (back) {
-				const unwrapped = unwrap(back);
-				unwrapped.slug = base + unwrapped.slug;
-				array[idx].flank = { back: unwrapped };
-			}
-			if (breakpoint && breakpoint(next)) return array;
-			if (next) {
-				if (!array[idx].flank) array[idx].flank = {};
-				const unwrapped = unwrap(next);
-				unwrapped.slug = base + unwrapped.slug;
-				// @ts-expect-error - `flank` definitely exists
-				array[idx].flank.next = unwrapped;
-			}
+	if (sort) items = items.sort(sort);
+	const array = /** @type {Array<T & Attachment>} */ (items);
+	const unwrap = ntv.pick(['slug', 'title']);
+	for (let idx = 0; idx < array.length; idx++) {
+		const [back, next] = [array[idx - 1], array[idx + 1]];
+		if (back) {
+			const unwrapped = unwrap(back);
+			unwrapped.slug = base + unwrapped.slug;
+			array[idx].flank = { back: unwrapped };
 		}
-		return array;
-	};
+		if (breakpoint && breakpoint(next)) return array;
+		if (next) {
+			if (!array[idx].flank) array[idx].flank = {};
+			const unwrapped = unwrap(next);
+			unwrapped.slug = base + unwrapped.slug;
+			// @ts-expect-error - `flank` definitely exists
+			array[idx].flank.next = unwrapped;
+		}
+	}
+	return array;
 }
