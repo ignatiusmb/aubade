18a5cc267c08fc666baba385c83237c953f7dba0
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-03-12 18:09:18 +0700

    fixes

--- workspace/aubade/src/compass/index.js
+++ workspace/aubade/src/compass/index.js
@@ -24,7 +24,7 @@ export function visit(entry) {
 export function scan(type, entry) {
 	/** @type {import('../types.js').HydrateChunk['siblings']} */
 	const entries = [];
-	for (const name of fs.readdirSync(entry)) {
+	for (const name of fs.statSync(entry).isDirectory() ? fs.readdirSync(entry) : []) {
 		const path = join(entry, name);
 		/** @type {any} - trick TS to enable discriminated union */
 		const stat = fs.statSync(path).isDirectory() ? 'directory' : 'file';
@@ -33,6 +33,7 @@ export function scan(type, entry) {
 		entries.push({
 			type: stat,
 			path,
+			breadcrumb: path.split(/[/\\]/).reverse(),
 			get buffer() {
 				return stat === 'file' ? fs.readFileSync(path) : void 0;
 			},
@@ -49,7 +50,7 @@ export function scan(type, entry) {
  */
 export function traverse(entry, { depth: level = 0 } = {}) {
 	const entries = scan('files', entry);
-	for (const { path } of level > 0 ? scan('directories', entry) : []) {
+	for (const { path } of level ? scan('directories', entry) : []) {
 		entries.push(...traverse(path, { depth: level - 1 }).files);
 	}
 
@@ -57,25 +58,31 @@ export function traverse(entry, { depth: level = 0 } = {}) {
 		files: entries,
 
 		/**
+		 * Hydrate `files` scanned on to the shelf with the `load` function.
+		 *
 		 * @template {object} Output
 		 * @param {(chunk: import('../types.js').HydrateChunk) => undefined | Output} load
-		 * @param {(path: string) => boolean} [files]
+		 * @param {(path: string) => boolean} [filter] files to process with `load`
+		 * @returns {Output[]}
 		 */
-		hydrate(load, files = (v) => v.endsWith('.md')) {
-			return entries.map(({ path, buffer }) => {
-				if (!files(path)) return [];
+		hydrate(load, filter = (v) => v.endsWith('.md')) {
+			const items = [];
+			for (const { path, breadcrumb, buffer } of entries) {
+				if (!filter(path)) continue;
 				const item = load({
-					breadcrumb: path.split(/[/\\]/).reverse(),
+					breadcrumb,
 					buffer,
 					marker,
 					parse,
 					get siblings() {
-						const tree = scan('all', path);
+						const parent = breadcrumb.slice(1).reverse();
+						const tree = scan('all', parent.join('/'));
 						return tree.filter(({ path: file }) => file !== path);
 					},
 				});
-				return item ?? [];
-			});
+				item && items.push(item);
+			}
+			return items;
 		},
 	};
 }
