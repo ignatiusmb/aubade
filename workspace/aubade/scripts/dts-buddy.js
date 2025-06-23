dbd3b8a8b38909433a5039d28a0125befc9c2f57
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-06-23 20:04:44 +0700

    chore: run scripts natively

--- workspace/aubade/scripts/dts-buddy.js
+++ workspace/aubade/scripts/dts-buddy.js
@@ -1,16 +1,16 @@
 import * as fs from 'node:fs';
 import { createBundle } from 'dts-buddy';
-import { exports } from '../package.json';
+import pkg from '../package.json' with { type: 'json' };
 
 await createBundle({
 	output: 'index.d.ts',
-	modules: Object.keys(exports).reduce((acc, key) => {
+	modules: Object.keys(pkg.exports).reduce((acc, key) => {
 		if (key.slice(2).includes('.')) return acc; // skip non-modules
-		return { ...acc, ['aubade' + key.slice(1)]: exports[key].default };
+		return { ...acc, ['aubade' + key.slice(1)]: pkg.exports[key].default };
 	}, {}),
 });
 
-for (const subpath of Object.keys(exports).filter((k) => k.includes('*'))) {
+for (const subpath of Object.keys(pkg.exports).filter((k) => k.includes('*'))) {
 	const parent = subpath.slice(0, subpath.lastIndexOf('/'));
 	const ext = subpath.slice(subpath.lastIndexOf('*') + 1);
 	const files = fs.readdirSync(parent).filter((f) => f.endsWith(ext));
