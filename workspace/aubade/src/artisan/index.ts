6f2a4849246a38dc70ac013ccff12a3029b0e667
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-07-08 14:16:45 +0700

    this fits better

--- workspace/aubade/src/artisan/index.ts
+++ workspace/aubade/src/artisan/index.ts
@@ -1,4 +1,4 @@
-export { extract as matter, type FrontMatter } from '../prelude/index.js';
+export { extract as matter, type FrontMatter } from '../manifest/index.js';
 export { markdown } from './markdown/index.js';
 export { marker } from './marker/index.js';
 export { highlighter, transform } from './palette/index.js';
